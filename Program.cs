b6b963deb0c0b54fde0688af7962d60a991605c5
rafaelferreira <rafael.fmelo@hotmail.com>
2023-11-18 16:25:13 -0300

    Enviando arquivos do projeto

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Inicio:
+
+            double R = 0;
+            double h = 0;
+
+            Console.Write("Digite o raio da base do cilindro: ");
+
+            R = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Digite a altura do cilindro: ");
+
+            h = double.Parse(Console.ReadLine());
+
+            //Cálculo do volume
+
+            double V = Math.PI * Math.Pow(R, 2) * h;
+
+            Console.WriteLine();
+
+            //Volume do Cílindro
+            Console.WriteLine($"Volume do cilindro: {V} ");
+
+            Console.WriteLine();
+
+        CalcularNovamente:
+            Console.Write("Deseja calcular novamente ? (s/n) : ");
+
+            string calcNovamente = Console.ReadLine();
+
+            Console.WriteLine();
+
+            switch (calcNovamente)
+            {
+                case "s":
+                    goto Inicio;
+                    break;
+
+                case "S":
+                    goto Inicio;
+                    break;
+
+                case "n":
+                    break;
+
+                case "N":
+                    break;
+
+                default:
+                    Console.WriteLine("Digite uma opção válida !");
+                    goto CalcularNovamente;
+                    break;
+            }
+        }
+    }
+}
